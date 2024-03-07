ec174d9391cc6b4e2c5d20860c8bd1c0eb2fad41
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-07 18:11:25 -0300

    Add files via upload

---
+++ Estoque/src/Produto.java
@@ -0,0 +1,32 @@
+package Estoque;
+
+public class Produto {
+
+    int quantidadeAtual;
+    int quantidadeMaxima;
+    int quantidadeMinima;
+
+    public int getQuantidadeAtual() {
+        return quantidadeAtual;
+    }
+
+    public void setQuantidadeAtual(int quantidadeAtual) {
+        this.quantidadeAtual = quantidadeAtual;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+}
