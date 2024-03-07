effb01271254f84c53ef0a42c5b2edddf10c70f7
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-07 18:33:37 -0300

    Add files via upload

---
+++ Triangulos/src/Triangulo.java
@@ -0,0 +1,31 @@
+package Triangulos;
+
+class triangulo {
+    int lado1;
+    int lado2;
+    int lado3;
+
+    public int getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(int lado1) {
+        this.lado1 = lado1;
+    }
+
+    public int getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(int lado2) {
+        this.lado2 = lado2;
+    }
+
+    public int getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(int lado3) {
+        this.lado3 = lado3;
+    }
+}
