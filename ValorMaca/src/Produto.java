2d7084765ae26f07214fb705963016729b6e9080
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-07 18:32:05 -0300

    Add files via upload

---
+++ ValorMaca/src/Produto.java
@@ -0,0 +1,15 @@
+package ValorMaca;
+
+public class Produto {
+    int Quantidade;
+
+
+    public int getQuantidade() {
+        return Quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        Quantidade = quantidade;
+    }
+
+}
