d84f8bdc74e56725411327093892a57b34e557f2
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-16 15:05:03 -0300

    Delete aula5/questao2 directory

--- aula5/questao2/src/Smartphone.java
+++
@@ -1,9 +0,0 @@
-public class Smartphone extends Produto {
-    public void ligacao(){
-        System.out.println("Estou fazendo uma ligação");
-    }
-
-    public Smartphone(String nome, int estoque, int preco) {
-        super(nome, estoque, preco);
-    }
-}
--- aula5/questao2/src/Televisao.java
+++
@@ -1,9 +0,0 @@
-public class Televisao extends Produto {
-    public void acessarCanais(){
-        System.out.println("Estou acessando canais");
-    }
-
-    public Televisao(String nome, int estoque, int preco) {
-        super(nome, estoque, preco);
-    }
-}
