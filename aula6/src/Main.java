f1a1b371dfceb738f35deef979e5ed395e369e92
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-16 15:37:20 -0300

    Delete aula6 directory

--- aula6/src/Main.java
+++
@@ -1,7 +0,0 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        Carro c = new Carro("Chevrolet", "Ônix", 2022);
-//    }
-//}
-//
-//
--- aula6/src/Main2.java
+++
@@ -1,7 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        Aluno aluno = new Aluno("João", 12345);
-        System.out.println("Nome: " + aluno.getNome());
-        System.out.println("Matrícula: " + aluno.getMat());
-    }
-}
