67f7db4c3d559e4d49ef59917a8bdcf486a27ea9
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-16 15:37:34 -0300

    Add files via upload

---
+++ aula6/questao1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("Ticiano", 12345);
+        System.out.println("Nome: " + aluno.getNome());
+        System.out.println("Matrícula: " + aluno.getMat());
+    }
+}
