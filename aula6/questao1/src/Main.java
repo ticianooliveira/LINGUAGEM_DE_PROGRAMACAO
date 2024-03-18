3ea245bd27e8a3e1b4f042b958f83c8a335a436c
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-18 18:15:29 -0300

    Delete aula6/questao1 directory

--- aula6/questao1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Aluno aluno = new Aluno("Ticiano", 12345);
-        System.out.println("Nome: " + aluno.getNome());
-        System.out.println("Matrícula: " + aluno.getMat());
-    }
-}
